18beff7e0b3a2a2b7c9e223c5d6e121fcdad45f6
Bruno Velasquez <velasquez.bruno@gmail.com>
2022-02-18 10:54:03 -0300

    First version of the DAGs and ETL file

--- dags/spotify_etl.py
+++ dags/spotify_etl.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-import pandas as pd 
+import pandas as pd
 from sqlalchemy.orm import sessionmaker
 import requests
 import json
@@ -15,7 +15,7 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
     # Check if dataframe is empty
     if df.empty:
         print("No songs downloaded. Finishing execution")
-        return False 
+        return False
 
     # Primary Key Check
     if pd.Series(df['played_at']).is_unique:
@@ -28,36 +28,36 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
         raise Exception("Null values found")
 
     # Check that all timestamps are of yesterday's date
-    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+    # yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    # yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
 
-    timestamps = df["timestamp"].tolist()
-    for timestamp in timestamps:
-        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
-            raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
+    # timestamps = df["timestamp"].tolist()
+    # for timestamp in timestamps:
+    #     if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+    #         raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
 
-    return True
+    # return True
 
 
 def run_spotify_etl():
     DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-    USER_ID = ''
-    TOKEN = ''
+    USER_ID = '12181041980'
+    TOKEN = 'BQAD-5-V0nSY6_RHFcRxdzTy2qKx9JRWS1XOF7Kfh4u2vDs2JCoBDoiBtvyW8MG6fTBIqY3fYODJ05fAWRrXz4jKhpEFWAu5t5jJ6mBNihGi0l7J95Dq-DTfbSrEroyggm9nI33L2hV1YTe7QRUH3Q'
 
       # Extract part of the ETL process
- 
+
     headers = {
         "Accept" : "application/json",
         "Content-Type" : "application/json",
         "Authorization" : "Bearer {token}".format(token=TOKEN)
     }
-    
-    # Convert time to Unix timestamp in miliseconds      
+
+    # Convert time to Unix timestamp in miliseconds
     today = datetime.datetime.now()
     yesterday = today - datetime.timedelta(days=1)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
-    # Download all songs you've listened to "after yesterday", which means in the last 24 hours      
+    # Download all songs you've listened to "after yesterday", which means in the last 24 hours
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
 
     data = r.json()
@@ -67,14 +67,20 @@ def run_spotify_etl():
     played_at_list = []
     timestamps = []
 
-    # Extracting only the relevant bits of data from the json object      
+    # Extracting only the relevant bits of data from the json object
     for song in data["items"]:
         song_names.append(song["track"]["name"])
         artist_names.append(song["track"]["album"]["artists"][0]["name"])
         played_at_list.append(song["played_at"])
         timestamps.append(song["played_at"][0:10])
-        
-    # Prepare a dictionary in order to turn it into a pandas dataframe below       
+
+    print(song_names)
+    print(artist_names)
+    print(played_at_list)
+    print(timestamps)
+
+
+    # Prepare a dictionary in order to turn it into a pandas dataframe below
     song_dict = {
         "song_name" : song_names,
         "artist_name": artist_names,
@@ -83,7 +89,7 @@ def run_spotify_etl():
     }
 
     song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
-    
+
     # Validate
     if check_if_valid_data(song_df):
         print("Data valid, proceed to Load stage")
@@ -108,7 +114,7 @@ def run_spotify_etl():
     print("Opened database successfully")
 
     try:
-        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+        song_df.to_sql("my_played_tracks", conn, index=False, if_exists='append')
     except:
         print("Data already exists in the database")
 
